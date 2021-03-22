dd7c59b7d2eb86e94def750def34db1a996842b3
Raad Saleh <xblaketrixx@gmail.com>
2021-03-22 13:46:42 +0600

    Lab1

---
+++ LAB 1/Cookie.php
@@ -0,0 +1,25 @@
+<?php
+$id="";
+$pass="";
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+    $id=$_POST["id"];
+    $pass=$_POST["pass"];
+    if($id == "ghost" && $pass == "asm01"){
+        header("Location: dash.php");
+    }
+}
+?>
+
+<html>
+<head></head>
+<body>
+<form method ="post">
+
+<input type="text" name="id" placeholder="ID"><br>
+<input type="password" name="pass" placeholder="Password"><br>
+<input type="submit" value="login">
+
+</form>
+
+</body>
+</html>
